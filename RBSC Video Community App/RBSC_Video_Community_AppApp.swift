c944fe5b0f9e6b4b0fa0faaa0c3593f98eabdaeb
Ruben Grimm <pmk1c@users.noreply.github.com>
2023-01-28 14:43:08 +0100

    Initial Commit

---
+++ RBSC Video Community App/RBSC_Video_Community_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RBSC_Video_Community_AppApp.swift
+//  RBSC Video Community App
+//
+//  Created by Ruben Grimm on 28.01.23.
+//
+
+import SwiftUI
+
+@main
+struct RBSC_Video_Community_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
