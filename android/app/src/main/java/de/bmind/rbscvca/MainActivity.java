eaa5d3e4509a0b47c9bf11f43093b11f64135c14
Ruben Grimm <pmk1c@users.noreply.github.com>
2023-08-17 19:58:40 +0200

    Fix app bundle id

--- android/app/src/main/java/de/bmind/rbscvca/MainActivity.java
+++ android/app/src/main/java/de/bmind/rbscvca/MainActivity.java
@@ -1,4 +1,4 @@
-package com.de.bmind.rbscvca;
+package de.bmind.rbscvca;
 
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
--- android/app/src/main/java/de/bmind/rbscvca/MainApplication.java
+++ android/app/src/main/java/de/bmind/rbscvca/MainApplication.java
@@ -1,4 +1,4 @@
-package com.de.bmind.rbscvca;
+package de.bmind.rbscvca;
 
 import android.app.Application;
 import com.facebook.react.PackageList;
