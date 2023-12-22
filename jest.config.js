c9eda4483d92adad9505e616e2520e07bdd7921c
Ruben Grimm <pmk1c@users.noreply.github.com>
2023-12-22 23:23:21 +0100

    Add jest configuration and first test

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,16 @@
 module.exports = {
-  preset: 'react-native',
+  preset: '@testing-library/react-native',
+  clearMocks: true,
+  fakeTimers: {
+    enableGlobally: true,
+  },
+  moduleNameMapper: {
+    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+      '<rootDir>/__mocks__/asset.ts',
+  },
+  randomize: true,
+  setupFilesAfterEnv: ['./__tests__/setup/failOnConsole.ts'],
+  transformIgnorePatterns: [
+    '<rootDir>/node_modules/(?!@react-native|react-native|react-native-pixel-perfect)',
+  ],
 };
