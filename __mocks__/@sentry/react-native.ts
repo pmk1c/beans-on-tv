a55db461511bf8b67bf27ebf8ef887d313419dec
Ruben Grimm <pmk1c@users.noreply.github.com>
2023-12-22 23:54:42 +0100

    Add GitHub CI QA and Test workflows (#32)

---
+++ __mocks__/@sentry/react-native.ts
@@ -0,0 +1,8 @@
+export const createReduxEnhancer = () => (next: unknown) => next;
+export const init = () => {};
+export const wrap = (component: unknown) => component;
+
+export class ReactNativeTracing {}
+export class ReactNavigationInstrumentation {
+  registerNavigationContainer() {}
+}
---
+++ __tests__/App.test.tsx
@@ -0,0 +1,11 @@
+import {render} from '@testing-library/react-native';
+
+import App from '../App';
+
+describe('App', () => {
+  it('renders', () => {
+    const tree = render(<App />).toJSON();
+
+    expect(tree).toMatchSnapshot();
+  });
+});
