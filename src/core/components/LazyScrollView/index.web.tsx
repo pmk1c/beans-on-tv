4c5b412cadc52fc0c5202e25b6ef3eeeb19b0724
Ruben Grimm <pmk1c@users.noreply.github.com>
2026-05-13 23:46:20 +0000

    fix checks

--- src/core/components/LazyScrollView/index.web.tsx
+++ src/core/components/LazyScrollView/index.web.tsx
@@ -1,5 +1,4 @@
 import { ScrollView, type ScrollViewProps } from "@expo/ui";
-import type { LazyVStackProps } from "@expo/ui/swift-ui";
 
 function LazyScrollView(props: ScrollViewProps) {
   return <ScrollView {...props} />;
