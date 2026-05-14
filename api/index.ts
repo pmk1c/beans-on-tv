e6c50fb29d57d4f5ce72649351b6edfd677869bd
Ruben Grimm <pmk1c@users.noreply.github.com>
2026-05-14 09:32:01 +0000

    use new expo-server lib

--- api/index.ts
+++ api/index.ts
@@ -1,4 +1,4 @@
-const { createRequestHandler } = require("@expo/server/adapter/vercel");
+const { createRequestHandler } = require("expo-server/adapter/vercel");
 
 module.exports = createRequestHandler({
   build: require("path").join(__dirname, "../dist/server"),
