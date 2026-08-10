7f877631a2d99e845a9998d2aab2a9590bb9e116
Ruben Grimm <pmk1c@users.noreply.github.com>
2026-08-10 20:55:08 +0000

    build prisma client

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -9,7 +9,7 @@ export const authClient = createAuthClient({
     expoClient({
       scheme: "beansontv",
       storagePrefix: "beansontv",
-      storage: typeof window === "undefined" ? SecureStore : window.localStorage,
+      storage: typeof window?.localStorage === "undefined" ? SecureStore : window.localStorage,
     }),
     deviceAuthorizationClient(),
     genericOAuthClient(),
